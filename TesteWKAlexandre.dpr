930489121ffcb9d380fce0e2aedc5e26ea4c7e77
Alexandre-x <asp.go@bol.com.br>
2024-10-24 17:23:34 -0300

    Classes Controller e Model concluidas

--- TesteWKAlexandre.dpr
+++ TesteWKAlexandre.dpr
@@ -3,7 +3,8 @@ program TesteWKAlexandre;
 uses
   Vcl.Forms,
   UPedidosVenda in 'View\UPedidosVenda.pas' {FormPedidosVenda},
-  UControllerPedidosVenda in 'Controller\UControllerPedidosVenda.pas';
+  UControllerPedidosVenda in 'Controller\UControllerPedidosVenda.pas',
+  UModelPedidosVenda in 'Model\UModelPedidosVenda.pas';
 
 {$R *.res}
 
